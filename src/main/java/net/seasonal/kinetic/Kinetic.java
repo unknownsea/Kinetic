dbf887951b062f90db20cfd59b0ee5270ab16efa
unknownsea <seasoank@gmail.com>
2025-02-06 17:43:16 -0700

    Add files via upload

---
+++ src/main/java/net/seasonal/kinetic/Kinetic.java
@@ -0,0 +1,15 @@
+package net.seasonal.kinetic;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Kinetic implements ModInitializer {
+	public static final String MOD_ID = "kinetic";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Kinetic initialized!");
+	}
+}
